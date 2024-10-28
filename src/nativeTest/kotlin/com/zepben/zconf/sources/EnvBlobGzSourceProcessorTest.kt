eae5acec8a3c99ea74099f4239bd5087e0abc4d1
Zara Kay <zara.kay@zepben.com>
2024-10-28 14:37:27 +1100

    Fix `env-blob-gz` does not work for compressed data larger than 1024 bytes (#7)
    Signed-off-by: Zara Kay <zara.kay@zepben.com>

--- src/nativeTest/kotlin/com/zepben/zconf/sources/EnvBlobGzSourceProcessorTest.kt
+++ src/nativeTest/kotlin/com/zepben/zconf/sources/EnvBlobGzSourceProcessorTest.kt
@@ -55,4 +55,11 @@ class EnvBlobGzSourceProcessorTest : FunSpec({
         val envVal = "H4sIAAAAAAAAA6tWyk3NK1WyUqjmUlBQykwBspTSMnNSlXRA/"
         EnvBlobGzSourceProcessor("FAKE") { _ -> envVal }.properties
     }
+
+    test ("it works on something bigger than the intermediate buffer") {
+        val envVal = "H4sIAAAAAAAA/6yTz47kJhDG36WOkXEbg/9xi3aj3TkkWWnmFu0BQ9GN1g1eKE9rNOp3j/BMtyJNDjlEPlhAFUX9vq9eQW90AlX+1mMwCArEJLreCM2MdoLJ3sxslnxg0zj3UrbdIK2ACs5Ip2hBAQZK2luogNKWCe1DzhumDOovOBGtWR0OSzz6UJ+9STFHRzEsPmBt4vnAeS+k6GbWi9ExOcuBjdOEDI0ZBs7NOAh+eG7rBr5fK7Ca9Kwzlifb5J8xgYKYjvUaMx0T5p9L/fltv4JttZrw0ZzwrP8Mj6QTgaK04bUCvMxvbX9o33DHZWMdm4bWMKndxMYGOZNNN3Ixi9k0LVRgFo+BHux/B+Z3LKD+Vyj/osO1glPMVHYucx2QLjH9OEeLSx1iWFO07Hk1tb7k2geCCtZYwEgpKlhTpGjiAgq+Pj19e4QKEv7cMNPvtzJffnuCCp4xeffyCRN5542mQs7pJWMp78MRM8VUyKYtkD/vghGe12UPfQWdsz+Gb9u8ePOwgtpzC9XioPSHLhmAJrP397O9AVZkq8DEQNqHe9y9XgUZzZY8vXxJcVsf7G7CfGRNx6ceh9bOUzP1AxfwvYK8zQHpFrQvWCOmYRSis1r3vJOmh+p+1KEd3dS53lnnumn6x5HtzdS5buZtN8wD1+V60vnHZ3Q+ePIx/JoCKNApKH3JCk1WemU5bnRCnYm1auz40HZSTqPgquQye08+fKDAbj0rWQbvZX3nVfC/r+6U4HotYNI+La83czT1/t31H/pOVnDB+dPu7K9vUb/A9fo3AAAA//8BAAD//w/eRjwmBAAA"
+        val config = EnvBlobGzSourceProcessor("FAKE") { _ -> envVal }.properties as ConfigObject
+
+        config["server.host"] shouldBe ConfigValue("0.0.0.0")
+    }
 })
